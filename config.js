2534c0332dbd12d52ebd84801ab47c5aecc63af6
rudokemper <rkemper@digital-democracy.org>
2021-11-17 18:23:01 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,223 @@
+var config = {
+    style: 'mapbox://styles/dd-mapeo/ckw44fy4214l214pbfaupc4ba',
+    accessToken: 'pk.eyJ1IjoiZGQtbWFwZW8iLCJhIjoiY2tuaGtxN2hjMjg5YTJ1b29kb3k4a3hwMCJ9.Q4cR8vp-Z6pP9PTUswy7Vw',
+    showMarkers: false,
+    theme: 'dark',
+    use3dTerrain: false,
+    title: 'Digital Democracy: Technology in Solidarity with Frontline Technologies',
+    logo: 'images/Digital Democracy.png',
+    bookmarks: '<strong>Bookmarks</strong>: <a href="#page1">Overview</a> | <a href="#page2">Territory mapping with the Waorani (Ecuador)</a> | <a href="#page3">Drone mapping with the Wapichan (Guyana)</a> | <a href="#page4">Monitoring with ECA-Amarakaeri (Peru)</a> | <a href="#page5">Monitoring with PUINAMUDT (Peru)</a> | <a href="#page6">Historical land tenure mapping with the Ogiek (Kenya)</a> | <a href="#page7">Vulnerability mapping with ODI (SE Asia)</a> | <a href="#page8">Biodiversity mapping with the Santo Sunset Environmental Network (Vanuatu)</a> | <a href="#page9">Mapping oral histories with Ohneganos (Canada) | <a href="#page10">Earth Defenders Toolkit and how to use our tools</a><div id="rotate-mobile"><em>For optimal viewing of this storytelling map on mobile, rotate your device to a horizontal orientation.</em><br><br><img src="images/device.png">',
+	footer: '<a href="https://digital-democracy.org" class="fas fa-link" target="_blank"></a>&nbsp;<a href="https://www.instagram.com/digidem/" class="fab fa-instagram" target="_blank"></a>&nbsp;<a href="http://twitter.com/DigiDem" class="fab fa-twitter" target="_blank"></a>&nbsp;<a href="https://www.facebook.com/digidemocracy/" class="fab fa-facebook-f" target="_blank"></a>&nbsp;<a href="https://www.linkedin.com/company/-digital-democracy/" class="fab fa-linkedin" target="_blank"></a>&nbsp;<a href="https://www.youtube.com/user/digitaldemocracycam" class="fab fa-youtube" target="_blank"></a>',
+    chapters: [
+        {
+            id: 'page1',
+            alignment: "left",
+            hidden: false,
+            title: 'Overview',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [154.76971, 16.69809], 
+				zoom: 1.5,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        },
+		        {
+            id: 'page2',
+            alignment: "left",
+            hidden: false,
+            title: 'Territory mapping with the Waorani (Ecuador)',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [-76.38612, -1.15350], 
+				zoom: 8.5,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        },
+		        {
+            id: 'page3',
+            alignment: "left",
+            hidden: false,
+            title: 'Drone mapping with the Wapichan (Guyana)',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [-59.56458, 2.52352], 
+				zoom: 8,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        },
+		        {
+            id: 'page4',
+            alignment: "left",
+            hidden: false,
+            title: 'Monitoring with ECA-Amarakaeri (Peru)',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [-72.84421,  -11.34042], 
+				zoom: 8.5,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        },
+		        {
+            id: 'page5',
+            alignment: "left",
+            hidden: false,
+            title: 'Monitoring with PUINAMUDT (Peru)',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [-74.73776, -2.60102], 
+				zoom: 7.5,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        },
+		        {
+            id: 'page6',
+            alignment: "left",
+            hidden: false,
+            title: 'Historical land tenure mapping with the Ogiek (Kenya)',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [34.56696, 1.12026], 
+				zoom: 9.75,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        },
+		        {
+            id: 'page7',
+            alignment: "left",
+            hidden: false,
+            title: 'Vulnerability mapping with ODI (SE Asia)',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [103.47793, 16.06399], 
+				zoom: 5.3,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        },
+		        {
+            id: 'page8',
+            alignment: "left",
+            hidden: false,
+            title: 'Biodiversity mapping with the Santo Sunset Environmental Network (Vanuatu)',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [166.72493, -15.21572], 
+				zoom: 9.0,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        },			
+		        {
+            id: 'page9',
+            alignment: "left",
+            hidden: false,
+            title: 'Mapping oral histories with Ohneganos (Canada)',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [-80.01420, 43.11287], 
+				zoom: 8.5,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        },
+		        {
+            id: 'page10',
+            alignment: "left",
+            hidden: false,
+            title: 'Earth Defenders Toolkit and how to use our tools',
+            image: 'images/',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            location: {
+                center: [154.76971, 16.69809], 
+				zoom: 1.5,
+				pitch: 0.00,
+				bearing: 0.00,
+            },
+            mapAnimation: "flyTo",
+            rotateAnimation: false,
+            callback: "",
+            onChapterEnter: [
+			],
+            onChapterExit: [
+            ]
+        }
+    ]
+};
