c65b8319aa085df5a7851057dba65a898c75b532
rudokemper <rkemper@digital-democracy.org>
2021-12-21 10:30:05 -0500

    add ohneganos, ogiek; authors element

--- config.js
+++ config.js
@@ -120,10 +120,11 @@ var config = {
             alignment: "left",
             hidden: false,
             title: 'Historical land tenure mapping with the Ogiek (Kenya)',
-            image: 'images/',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            image: 'images/ogiek.jpg',
+            author: 'Rudo Kemper, Digital Democracy',
+            description: 'The Ogiek are one of Africa’s last remaining forest dwellers and hunter-gatherer communities. Traditionally honey-gatherers, they survive mainly on wild fruits and roots, game hunting and traditional beekeeping. Since the beginning of the twentieth century, the Ogiek at Mount Elgon in Kenya have been routinely subjected to arbitrary forced evictions from their ancestral lands, and having their lands allocated to agricultural settlers or national parks. The Chepkitale Indigenous Peoples Development Project (CIPDP) is an Ogiek community organization working to advocate for the recovery of land rights of Ogiek ancestral lands.<br><br>Digital Democracy is helping the Ogiek community in using Mapeo to collect and have local ownership over data about their ancestral lands to support the advocacy processes led by CIPDP. In partnership with the Forest Peoples Programme, we are providing remote accompaniment to help build a bespoke Mapeo configuration for the Ogiek and train a dedicated team of CIPDP mappers to use Mapeo, so they can go on and train others in the community, and initiate a participatory mapping process across the ancestral territory. The hope is that the data and maps generated, along with solidarity building during the process, will empower the Ogiek to negotiate for their land rights, and also clarify boundaries between the lands of other neighboring communities.',
             location: {
-                center: [34.56696, 1.12026], 
+                center: [34.56696, 1.026], 
 				zoom: 9.75,
 				pitch: 0.00,
 				bearing: 0.00,
@@ -132,8 +133,40 @@ var config = {
             rotateAnimation: false,
             callback: "",
             onChapterEnter: [
+                {
+                    layer: 'ogiek-1',
+                    opacity: 0.5
+                },
+                {
+                    layer: 'ogiek-2',
+                    opacity: 1
+                },
+                {
+                    layer: 'ogiek-label-1',
+                    opacity: 1
+                },
+                {
+                    layer: 'ogiek-label-2',
+                    opacity: 1
+                },
 			],
             onChapterExit: [
+                {
+                    layer: 'ogiek-1',
+                    opacity: 0
+                },
+                {
+                    layer: 'ogiek-2',
+                    opacity: 0
+                },
+                {
+                    layer: 'ogiek-label-1',
+                    opacity: 0
+                },
+                {
+                    layer: 'ogiek-label-2',
+                    opacity: 0
+                },
             ]
         },
 		        {
@@ -183,11 +216,12 @@ var config = {
             alignment: "left",
             hidden: false,
             title: 'Mapping oral histories with Ohneganos (Canada)',
-            image: 'images/',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus, purus eget posuere efficitur, neque massa lobortis lorem, at consectetur velit nisi eu diam. Nam elementum eu ex sit amet ornare. Quisque lacinia risus libero, ultricies commodo sem auctor sed. Proin feugiat, tellus non scelerisque tempus, elit dolor luctus ligula, iaculis pretium nisi nisi in urna. Nam ac libero id mi mattis malesuada non eu magna. Proin vel lacus libero. Quisque scelerisque erat vitae ex vulputate, eu vulputate nulla tempus. Praesent sit amet nunc massa. In gravida hendrerit ullamcorper. Maecenas pharetra a augue vitae suscipit. Praesent sed porttitor est. Aliquam consequat condimentum dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            image: 'images/ohneganos.jpg',
+            author: 'Rudo Kemper, Digital Democracy',
+            description: 'Six Nations is the largest First Nations reserve in Canada. It is the only reserve in North America that has representatives of all six Haudenosaunee nations living together. These nations are the Mohawk, Cayuga, Onondaga, Oneida, Seneca and Tuscarora. Some Lenape (formerly known as Delaware) also live in the territory. The Haudenosaunee (or Iroquois) people originally inhabited a much larger area stretching across the United Stated and Canada, and were legally granted a large tract of land by the French via the 1784 Haldimand Treaty; this was later taken away, and Six Nations represents only 5% of that area. The organization Ohneganos is an Indigenous water research program led by McMaster University professor Dr. Dawn Martin Hill, and are working to co-create Indigenous water quality tools, and mixed-method tools for collecting and training in Indigenous ecological knowledge.<br><br>As part of the Indigenous ecological knowledge research program of Ohneganos, Digital Democracy is providing programmatic support to Ohneganos in using Mapeo to collect Indigenous ecological knowledge across Six Nations, and recording place-based oral histories about important Haudenosaunee cultural sites across the lands. The Ohneganos team, together with a local mapping coordinator, will be using both Mapeo Mobile and Desktop to gather and organize data, and will be mapping and visualizing the geospatial data and stories using the open-source application Terrastories. This project represents the first pilot in testing out a Mapeo-to-Terrastories workflow.',
             location: {
-                center: [-80.01420, 43.11287], 
-				zoom: 8.5,
+                center: [-80.31420, 43.41287], 
+				zoom: 7.5,
 				pitch: 0.00,
 				bearing: 0.00,
             },
@@ -195,8 +229,24 @@ var config = {
             rotateAnimation: false,
             callback: "",
             onChapterEnter: [
+                {
+                    layer: 'ohneganos-1',
+                    opacity: 0.5
+                },
+                {
+                    layer: 'ohneganos-label-1',
+                    opacity: 1
+                },
 			],
             onChapterExit: [
+                {
+                    layer: 'ohneganos-1',
+                    opacity: 0
+                },
+                {
+                    layer: 'ohneganos-label-1',
+                    opacity: 0
+                },
             ]
         },
 		        {
