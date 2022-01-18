6ab974fc38a49e040425fa5d1a064299a32f56fe
rudokemper <rkemper@digital-democracy.org>
2022-01-18 18:15:00 -0500

    Cartography updates

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ var config = {
             description: 'Digital Democracy partners with marginalized communities worldwide to build tools to defend their  environmental and human rights. We envision a healthy, vibrant planet where all people are able to participate in the decisions that govern their lives.<br><br>Our work lies in deep, trusting relationships with local partners. Digital Democracy supports communities who are working to defend their human and environmental rights, but who lack the tools to do so. Our local partners represent marginalized communities worldwide, including Indigenous Peoples and ethnic minorities, whose human rights are most severely impacted by environmental abuse. We train partners to use basic digital tools to gather, share, and manage information about environmental and human rights infractions.<br><br>Based on years of work with local communities, Digital Democracy observed persistent technological gaps that weren’t being met by corporate technologies. In 2013 we began to co-create tech solutions with our partners in the Amazon to develop a tool that would meet their needs, resulting in Mapeo, an open-source tool for frontline defenders.',
                 location: {
                 center: [154.76971, 16.69809], 
-				zoom: 1.5,
+				zoom: 2,
 				pitch: 0.00,
 				bearing: 0.00,
             },
@@ -27,6 +27,30 @@ var config = {
             rotateAnimation: false,
             callback: "",
             onChapterEnter: [
+                {
+                    layer: "state-label",
+                    opacity: 0,
+                },
+                {
+                    layer: "settlement-major-label",
+                    opacity: 0,
+                },
+                {
+                    layer: "settlement-minor-label",
+                    opacity: 0,
+                },
+                {
+                    layer: "settlement-subdivision-label",
+                    opacity: 0,
+                },
+                {
+                    layer: "dd-workareas",
+                    opacity: 1,
+                },
+                {
+                    layer: 'admin-0-boundary',
+                    opacity: 0
+                },      
 			],
             onChapterExit: [
             ]
@@ -49,6 +73,58 @@ var config = {
             rotateAnimation: false,
             callback: "",
             onChapterEnter: [
+                {
+                    layer: "state-label",
+                    opacity: 1,
+                },
+                {
+                    layer: "settlement-major-label",
+                    opacity: 1,
+                },
+                {
+                    layer: "settlement-minor-label",
+                    opacity: 1,
+                },
+                {
+                    layer: "settlement-subdivision-label",
+                    opacity: 1,
+                },
+                {
+                    layer: 'admin-0-boundary',
+                    opacity: 1
+                },          
+                {
+                    layer: "dd-workareas",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-reserve-label",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-community-labels",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-puestos",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-minero",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-reserve",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-community",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-buffer",
+                    opacity: 0,
+                }
 			],
             onChapterExit: [
             ]
@@ -62,8 +138,8 @@ var config = {
             author: 'Mar Cártro, Digital Democracy',
             description: 'In the Peruvian Madre de Dios department, the Harakbut, Matsiguenka, and Yine Peoples have monitored and protected their ancestral territories for centuries and consider themselves as owners and guards of this part of the Amazon. In 2002, following 18 years of constant struggle, part of their ancestral territory was recognized as a natural protected area, called the Amarakaeri Communal Reserve. Since 2006, the reserve is co-managed between ten Indigenous communities (organized by the ECA Amarakaeri organization) and the National Service of Protected Areas (SERNANP), with the support of two Indigenous organizations (FENAMAD and COHARYIMA). Community guards, park rangers and technicians use Mapeo, together with other state-of-the-art Technology, to protect their ancestral forests from illegal mining and logging. Their exemplary efforts have been recognized by the Equator Prize in 2019. Moreover, the IUCN included the Amarakaeri Communal Reserve in their Green list because of its high conservation status.',
             location: {
-                center: [-72.84421,  -11.34042], 
-				zoom: 8.5,
+                center: [-71.284, -12.768], 
+				zoom: 9,
 				pitch: 0.00,
 				bearing: 0.00,
             },
@@ -71,6 +147,38 @@ var config = {
             rotateAnimation: false,
             callback: "",
             onChapterEnter: [
+                {
+                    layer: "eca-reserve-label",
+                    opacity: 1,
+                },
+                {
+                    layer: "eca-community-labels",
+                    opacity: 1,
+                },
+                {
+                    layer: "eca-puestos",
+                    opacity: 1,
+                },
+                {
+                    layer: "eca-minero",
+                    opacity: 1,
+                },
+                {
+                    layer: "eca-reserve",
+                    opacity: 1,
+                },
+                {
+                    layer: "eca-community",
+                    opacity: 1,
+                },
+                {
+                    layer: "eca-buffer",
+                    opacity: 1,
+                },
+                {
+                    layer: 'admin-0-boundary',
+                    opacity: 1
+                }    
 			],
             onChapterExit: [
             ]
@@ -85,7 +193,7 @@ var config = {
             description: 'The Ogiek are one of Africa’s last remaining forest dwellers and hunter-gatherer communities. Traditionally honey-gatherers, they survive mainly on wild fruits and roots, game hunting and traditional beekeeping. Since the beginning of the twentieth century, the Ogiek at Mount Elgon in Kenya have been routinely subjected to arbitrary forced evictions from their ancestral lands, and having their lands allocated to agricultural settlers or national parks. The Chepkitale Indigenous Peoples Development Project (CIPDP) is an Ogiek community organization working to advocate for the recovery of land rights of Ogiek ancestral lands.<br><br>Digital Democracy is helping the Ogiek community in using Mapeo to collect and have local ownership over data about their ancestral lands to support the advocacy processes led by CIPDP. In partnership with the Forest Peoples Programme, we are providing remote accompaniment to help build a bespoke Mapeo configuration for the Ogiek and train a dedicated team of CIPDP mappers to use Mapeo, so they can go on and train others in the community, and initiate a participatory mapping process across the ancestral territory. The hope is that the data and maps generated, along with solidarity building during the process, will empower the Ogiek to negotiate for their land rights, and also clarify boundaries between the lands of other neighboring communities.',
             location: {
                 center: [34.56696, 1.026], 
-				zoom: 9.75,
+				zoom: 9.95,
 				pitch: 0.00,
 				bearing: 0.00,
             },
@@ -109,6 +217,46 @@ var config = {
                     layer: 'ogiek-label-2',
                     opacity: 1
                 },
+                {
+                    layer: 'admin-0-boundary',
+                    opacity: 1
+                },        
+                {
+                    layer: 'settlement-minor-label',
+                    opacity: 1
+                },       
+                {
+                    layer: 'state-label',
+                    opacity: 1
+                },
+                {
+                    layer: "eca-reserve-label",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-community-labels",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-puestos",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-minero",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-reserve",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-community",
+                    opacity: 0,
+                },
+                {
+                    layer: "eca-buffer",
+                    opacity: 0,
+                }
 			],
             onChapterExit: [
                 {
@@ -138,8 +286,8 @@ var config = {
             author: 'Rudo Kemper, Digital Democracy',
             description: 'Six Nations is the largest First Nations reserve in Canada. It is the only reserve in North America that has representatives of all six Haudenosaunee nations living together. These nations are the Mohawk, Cayuga, Onondaga, Oneida, Seneca and Tuscarora. Some Lenape (formerly known as Delaware) also live in the territory. The Haudenosaunee (or Iroquois) people originally inhabited a much larger area stretching across the United Stated and Canada, and were legally granted a large tract of land by the French via the 1784 Haldimand Treaty; this was later taken away, and Six Nations represents only 5% of that area. The organization Ohneganos is an Indigenous water research program led by McMaster University professor Dr. Dawn Martin Hill, and are working to co-create Indigenous water quality tools, and mixed-method tools for collecting and training in Indigenous ecological knowledge.<br><br>As part of the Indigenous ecological knowledge research program of Ohneganos, Digital Democracy is providing programmatic support to Ohneganos in using Mapeo to collect Indigenous ecological knowledge across Six Nations, and recording place-based oral histories about important Haudenosaunee cultural sites across the lands. The Ohneganos team, together with a local mapping coordinator, will be using both Mapeo Mobile and Desktop to gather and organize data, and will be mapping and visualizing the geospatial data and stories using the open-source application Terrastories. This project represents the first pilot in testing out a Mapeo-to-Terrastories workflow.',
             location: {
-                center: [-80.31420, 43.41287], 
-				zoom: 7.5,
+                center: [-79.31420, 43.61287], 
+				zoom: 6.5,
 				pitch: 0.00,
 				bearing: 0.00,
             },
@@ -149,12 +297,37 @@ var config = {
             onChapterEnter: [
                 {
                     layer: 'ohneganos-1',
-                    opacity: 0.5
+                    opacity: 1
+                },
+                {
+                    layer: 'ohneganos-2',
+                    opacity: 1
                 },
                 {
                     layer: 'ohneganos-label-1',
                     opacity: 1
                 },
+                {
+                    layer: 'ohneganos-label-2',
+                    opacity: 1
+                },
+                {
+                    layer: 'ohneganos-label-3',
+                    opacity: 1
+                },
+                {
+                    layer: 'admin-0-boundary',
+                    opacity: 1
+                },
+                {
+                    layer: 'settlement-minor-label',
+                    opacity: 0
+                },
+                {
+                    layer: 'state-label',
+                    opacity: 0
+                },
+
 			],
             onChapterExit: [
                 {
@@ -162,7 +335,19 @@ var config = {
                     opacity: 0
                 },
                 {
+                    layer: 'ohneganos-2',
+                    opacity: 0
+                },
+                {
                     layer: 'ohneganos-label-1',
+                    opacity: 0
+                },
+                {
+                    layer: 'ohneganos-label-2',
+                    opacity: 0
+                },
+                {
+                    layer: 'ohneganos-label-3',
                     opacity: 0
                 },
             ]
